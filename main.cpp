4d373740d24c75105cbf604ffb114cd2b784be2f
Tony <tonyng147@gmail.com>
2023-09-02 00:50:40 +0700

    add comment

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 
 #include <gst/gst.h>
-
+#include <string>
+#ifndef VIDEO_URI   // Define this Macro in CMAKE
+    const std::string videoURI = "file:///home/tony/Desktop/VideoRenderQt/Resources/Video/Sample.mp4";
+#else
+    const std::string videoURI = VIDEO_URI;
+#endif
 int main(int argc, char** argv){
-    GMainLoop* loop;
 
-    GstElement* pl;
+    GMainLoop* loop = nullptr;              // Create the loop for playing video
 
-    gst_init(&argc, &argv);
-    pl = gst_element_factory_make("playbin", "bin");
+    GstElement* pipeline = nullptr;         // Pipe acts as if playbin (source, sink)
 
-    g_object_set(pl, "uri", "file:///home/tony/Desktop/VideoRenderQt/Resources/Video/Sample.mp4", NULL);
+    gst_init(&argc, &argv);                 // Init the environment
+ 
+    pipeline = gst_element_factory_make("playbin","pipeline");   // Create playbin from factory
 
-    gst_element_set_state(pl, GST_STATE_PLAYING);
+    g_object_set(pipeline, "uri", videoURI.c_str(), NULL);       // Set URI for playbin
 
-    loop = g_main_loop_new(NULL, false);
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);          // Switch to playing state
+
+    loop = g_main_loop_new(NULL,false);
 
     g_main_loop_run(loop);
+
 };
